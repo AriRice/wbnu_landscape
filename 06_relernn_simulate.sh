81326cd19ae2b35a533a06770b00660a977a4c22
Joe Manthey <jdmanthey@gmail.com>
2022-03-03 16:51:44 -0600

    Create 06_relernn_simulate.sh

---
+++ 06_relernn_simulate.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=relernn
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --partition matador
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=9G
+#SBATCH --gpus-per-node=2
+
+source activate recomb
+
+# used the Manthey et al. 2021 Certhia mutation rate
+
+SEED="42"
+MU="2.506e-9"
+DIR="./relernn_output/"
+VCF="./nuthatch_relernn.vcf"
+GENOME="./wbnu.genome.bed"
+GENTIME="2"
+
+# Simulate data
+ReLERNN_SIMULATE \
+    --vcf ${VCF} \
+    --genome ${GENOME} \
+    --projectDir ${DIR} \
+    --assumedMu ${MU} \
+    --seed ${SEED} \
+    --assumedGenTime ${GENTIME} \
+    --unphased
+    
+    
